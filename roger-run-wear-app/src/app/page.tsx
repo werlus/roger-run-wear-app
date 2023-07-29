c799e63d48355940afc891b0c22facbd51453562
Justin Saranovic <jsaranovic@gmail.com>
2023-07-28 21:55:35 -0400

    Removed div to display weather data, added console log to get weatherData object

--- roger-run-wear-app/src/app/page.tsx
+++ roger-run-wear-app/src/app/page.tsx
@@ -15,6 +15,8 @@ export default function Home() {
         console.error(error);
       });
   }, []);
+
+  console.log(weatherData)
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -25,7 +27,6 @@ export default function Home() {
         </p>
       </div>
       <div>
-        {weatherData.weather}
       </div>
     </main>
   )
