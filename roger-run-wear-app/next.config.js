dae57d3a91285bf4f7615896998e50661775123d
Justin Saranovic <jsaranovic@gmail.com>
2023-07-30 20:35:40 -0400

    Updated env variable for openWeather API key

--- roger-run-wear-app/next.config.js
+++ roger-run-wear-app/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    env: {
+      openWeather: '44799e9520761c6e3e5f6c6ed0981881',
+    }
+}
